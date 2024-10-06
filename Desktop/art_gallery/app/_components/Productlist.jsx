84d4295b8a20222fafaa1652d8e8c0750b3988d6
mustafahussienmohmed <ju3213445@gmail.com>
2024-10-06 22:10:22 +0300

    Update changes for version 1

---
+++ Desktop/art_gallery/app/_components/Productlist.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Productlist() {
+return (
+
+	<div> Productlist </div>
+
+)
+	
+}
+export default list
---
+++ Desktop/art_gallery/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Hero from "./_components/Hero";
+import productsection from "./_components/productsection";
+export default function Home() {
+  return (
+    <div>
+      <Hero/>
+      < ProductSection/>
+    </div>
+  );
+}
